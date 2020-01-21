7d0315dbdea6c041c53b93a02e5c47a23687050d
Benjamin Sullivan <benjamin.r.sullivan@gmail.com>
2020-01-21 04:28:00 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,7 +6,11 @@ def nyc_pigeon_organizer(data)
   lives = ""
   
   for each in pigeon_names
-    pigeon[:]
+    pigeon[each] => {}
     if data[:color][:purple].include?(each)
-  
+      color.push("purple")
+    if data[:color][:grey].include?(each)
+      color.push("grey")
+    if data[:color][:white].include?(each)
+      color.push("white")
 end
